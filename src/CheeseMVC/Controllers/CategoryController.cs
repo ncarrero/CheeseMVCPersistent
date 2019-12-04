cde9570dc6a372c3d9ec78cb821d3c6843b19292
Nicole Carrero <ncarrero18@gmail.com>
2019-12-03 19:42:39 -0500

    completion of studio 1

---
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CheeseMVC.Data;
+using CheeseMVC.Models;
+using CheeseMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CheeseMVC.Controllers
+{
+        //create private field context of type CheeseDbContext
+        //object will be mechanism of which programmer interacts with objects stored in database
+        //MVC framework will do work of creating an instance of CheeseDbContext and passing into controller's constructor
+        //***need to add this code to every controller class that needs to have access to persistent collections defined in CheeseDbContext**
+    public class CategoryController : Controller
+    {
+        private readonly CheeseDbContext context;
+
+        public CategoryController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+        public IActionResult Index()
+        {
+            List<CheeseCategory> categories = context.Categories.ToList();
+            return View(categories);
+        }
+
+        public IActionResult Add()
+        {
+            AddCategoryViewModel addcategoryviewmodel = new AddCategoryViewModel();
+            return View(addcategoryviewmodel);
+        }
+
+        //create parameter and pass via model binding
+        [HttpPost]
+        public IActionResult Add(AddCategoryViewModel addCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                //add new category to existing categories
+                CheeseCategory newCategory = new CheeseCategory
+                {
+                    Name = addCategoryViewModel.Name
+                };
+
+                context.Categories.Add(newCategory);
+                context.SaveChanges();
+
+                return Redirect("/Category");
+            }
+
+            return View(addCategoryViewModel);
+        }
+    }
+}
--- src/CheeseMVC/Data/CheeseDbContext.cs
+++ src/CheeseMVC/Data/CheeseDbContext.cs
@@ -6,8 +6,11 @@ namespace CheeseMVC.Data
     public class CheeseDbContext : DbContext
     {
         public DbSet<Cheese> Cheeses { get; set; }
+        //do not need to create a new dbContext because they are both related to each other
+        //creates a separate table to reference categories
+        public DbSet<CheeseCategory> Categories { get; set; }
 
-        public CheeseDbContext(DbContextOptions<CheeseDbContext> options) 
+        public CheeseDbContext(DbContextOptions<CheeseDbContext> options)
             : base(options)
         { }
 
